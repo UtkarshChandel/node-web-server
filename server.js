966bfbd43585bcfdd8337b63102fa851846bc691
UtkarshChandel <personal.utkarsh24@gmail.com>
2017-01-11 23:36:41 +0530

    Add Project page

--- server.js
+++ server.js
@@ -18,6 +18,13 @@ app.get('/', (req,res) => {
 
     });
 
+app.get('/projects',(req,res) => {
+  res.render('projects.hbs',{
+    pageTitle : "Projects"
+  });
+});
+
+
 app.get('/about',(req,res)=>{
   res.render('about.hbs',{
     pageTitle: 'About Page',
