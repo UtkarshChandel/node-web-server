51c71c261a1623c85ca8d43546280924f6e57d4c
UtkarshChandel <personal.utkarsh24@gmail.com>
2017-01-11 11:15:30 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials'); 
+app.set('view engine',hbs);
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', (req,res) => {
+    res.render('home.hbs',{
+      pageTitle: 'Home Page',
+      currentYear : new Date().getFullYear(),
+      DisplayText : 'Welcome to Home Page'
+    });
+
+    });
+
+app.get('/about',(req,res)=>{
+  res.render('about.hbs',{
+    pageTitle: 'About Page',
+    currentYear : new Date().getFullYear()
+  });
+});
+
+app.get('/bad',(req,res)=>{
+  res.send({
+    errorMessage : 'Unable to handle req.'
+  });
+});
+
+app.listen(3000,console.log("Server is Up and Running on port:3000"));
